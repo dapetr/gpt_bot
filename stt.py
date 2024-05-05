b56eafd7cc22298b6cc8977995b45d40d18664cd
Your Name <miss.petrovadaria@yandex.ru>
2024-05-05 19:27:10 +0300

    stt

--- stt.py
+++ stt.py
@@ -28,5 +28,6 @@ def speech_to_text(data: bytes):
     if decoded_data.get("error_code") is None:
         return True, decoded_data.get("result")
     else:
-        logging.debug(f"При запросе в SpeechKit возникла ошибка ({decoded_data.get("error_code")})")
+        error_code = decoded_data.get("error_code")
+        logging.debug(f"При запросе в SpeechKit возникла ошибка ({error_code})")
         return False, "При запросе в SpeechKit возникла ошибка"
